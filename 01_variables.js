cf7041e4631c8d85c86da8881a2c297573ff0395
SHAHID-BOOP <mohdshahida891@gmail.com>
2024-08-30 20:06:56 +0530

    add javascript files and readme

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+
+const  accountId = 14453
+let accountEmail = "mohdshahida891@gmail.com"
+var accountPassword = "12234" 
+accountcity = "lucknow"
+let accountstate;
+
+// accountId = 4 //  not allowed
+// Prefer not to use var because of issue in block scope and functional scope
+
+accountEmail = "sgushjb"
+accountPassword = "s7636"
+accountcity = "kolkata"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountcity,accountstate])
